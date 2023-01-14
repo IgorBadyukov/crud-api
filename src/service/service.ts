b52982f90c03302ec07c20fd68caf13f62c57325
Igor Badyukov <badyukov1806302003@gmail.com>
2023-01-14 12:18:35 +0300

    feat: add middleware

--- src/service/service.ts
+++ src/service/service.ts
@@ -1,87 +1,20 @@
 import http from 'http';
-import { UserController } from '../controllers/userController';
-import { checkBodyReq, getReqData, uuidValidateV4 } from '../models/utils';
-import { IBody, IUser } from '../models/models';
+import {createUser, deleteUser, getUser, getUsers, updateUser} from "./middleware";
 
 export function runServer(PORT: number) {
   return http.createServer(async (req, res) => {
     try {
       // GET all users
       if (req.url === '/api/users' && req.method === 'GET') {
-        const users = await UserController.getUsers();
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(users));
+        await getUsers(req, res);
       } else if (req.url?.match(/^(\/api\/users)\/[0-9a-z-]+\/?$/) && req.method === 'GET') { // GET user
-        const id = req.url?.split('/api/users/')[1] as string;
-        if (!uuidValidateV4(id)) {
-          res.writeHead(400, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ message: 'UserId is invalid' }));
-        } else {
-          try {
-            const user = await UserController.getUser(id);
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(user));
-          } catch (error) {
-            res.writeHead(404, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'User doesn\'t exist!' }));
-          }
-        }
+        await getUser(req, res)
       } else if (req.url?.match(/^(\/api\/users)\/?$/) && req.method === 'POST') { // POST create new user
-        let bodyUser = await getReqData(req);
-        try {
-          bodyUser = JSON.parse(bodyUser as string);
-          if (!checkBodyReq(bodyUser as object)) {
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Error in body request' }));
-          } else {
-            const newUser = await UserController.createUser(bodyUser as IUser);
-            res.writeHead(201, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(newUser));
-          }
-        } catch (error) {
-          res.writeHead(400, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ message: 'Error in body request' }));
-        }
+        await createUser(req, res);
       } else if (req.url?.match(/^(\/api\/users)\/[0-9a-z-]+\/?$/) && req.method === 'PUT') { // PUT update user
-        const id = req.url?.split('/api/users/')[1] as string;
-        let bodyUser = await getReqData(req);
-        try {
-          bodyUser = JSON.parse(bodyUser as string);
-          if (!uuidValidateV4(id)) {
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'UserId is invalid' }));
-          } else if (!checkBodyReq(bodyUser as object)) {
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'Error in body request' }));
-          } else {
-            try {
-              const user = await UserController.updateUser(id, bodyUser as IBody);
-              res.writeHead(200, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify(user));
-            } catch {
-              res.writeHead(404, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify({ message: 'User doesn\'t exist!' }));
-            }
-          }
-        } catch {
-          res.writeHead(400, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ message: 'Error in body request' }));
-        }
+        await updateUser(req, res);
       } else if (req.url?.match(/^(\/api\/users)\/[0-9a-z-]+\/?$/) && req.method === 'DELETE') { // DELETE delete user
-        try {
-          const id = req.url?.split('/api/users/')[1] as string;
-          if (!uuidValidateV4(id)) {
-            res.writeHead(400, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message: 'UserId is invalid' }));
-          } else {
-            const message = await UserController.deleteUser(id);
-            res.writeHead(204, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ message }));
-          }
-        } catch (error) {
-          res.writeHead(404, { 'Content-Type': 'application/json' });
-          res.end(JSON.stringify({ message: (error as Error).message }));
-        }
+        await deleteUser(req, res);
       } else { // Not found
         res.writeHead(404, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify({ message: 'Page not found' }));
